ec5c20589d162c56cad1ae883e563b47cb7e998c
slhun22 <105125575+slhun22@users.noreply.github.com>
2024-05-14 00:43:09 +0900

    add: Spring Folder

---
+++ src/main/java/com/example/htmxTest/HtmxTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.htmxTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmxTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmxTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/htmxTest/HtmxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.htmxTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmxTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
