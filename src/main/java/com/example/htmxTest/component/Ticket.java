a1904c4781b3626d1b2378d8cc75e1a8a8bbdd37
slhun22 <105125575+slhun22@users.noreply.github.com>
2024-05-17 20:46:14 +0900

    fix : ticketpage.html

---
+++ src/main/java/com/example/htmxTest/component/Ticket.java
@@ -0,0 +1,13 @@
+package com.example.htmxTest.component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ticket {
+}
